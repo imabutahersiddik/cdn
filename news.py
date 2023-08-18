7d51ce67a7b3e5a53a9dabb1e57ba49ec60f0a9f
Abu Taher Siddik <138387257+imabutahersiddik@users.noreply.github.com>
2023-08-19 03:49:06 +0600

    Add files via upload

---
+++ news.py
@@ -0,0 +1,41 @@
+import openai
+import requests
+import json
+import os
+from github import Github
+
+# Set up the OpenAI API
+openai.api_key = os.environ["sk-MKEmkPoqS4Edt5hwCVYmb4s2wA8IvIBAxcKiF8XqOZz8HRhb"]
+openai.api_base = "https://api.chatanywhere.cn"
+
+# Set up the GitHub API
+g = Github("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")
+repo = g.get_repo("cdn")
+
+# Scrape the latest earthquake data
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+response = requests.get(url)
+data = response.json()
+
+# Extract the earthquake data
+earthquake = data["features"][0]["properties"]
+magnitude = earthquake["mag"]
+place = earthquake["place"]
+time = earthquake["time"]
+
+# Generate a news article using GPT-3
+prompt = f"Write a news article about a {magnitude} magnitude earthquake that occurred in {place} at {time}."
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=2048,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+article = response.choices[0].text
+
+# Create a new GitHub issue with the news article
+title = f"{magnitude} magnitude earthquake in {place}"
+body = f"An earthquake with a magnitude of {magnitude} occurred in {place} at {time}. Here is the news article:\n\n{article}"
+repo.create_issue(title=title, body=body)
